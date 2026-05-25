90909f52fd2f3e0e30650dd8b24d2522552b364a
tolibcyber <tolibcyber@gmail.com>
2026-05-26 04:41:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ filters,
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7288739341"))
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://example.com")
 
 # =========================
